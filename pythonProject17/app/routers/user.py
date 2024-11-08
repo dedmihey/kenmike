06646ebb66677fd8ec883491ef09f7679817e9ed
dedmihey <164859123+dedmihey@users.noreply.github.com>
2024-11-08 16:15:03 +0300

    Add files via upload

--- pythonProject17/app/routers/user.py
+++ pythonProject17/app/routers/user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from app.backend.db_depends import get_db
 from typing import Annotated
-from app.models import User
+from app.models.user import User
 from app.schemas import CreateUser, UpdateUser
 from sqlalchemy import insert, select, update, delete
 from slugify import slugify
@@ -49,21 +49,21 @@ async def update_user(db: Annotated[Session, Depends(get_db)], update_user: Upda
             status_code=status.HTTP_404_NOT_FOUND,
             detail='User was not found'
         )
-    db.execute(update(User).values(firstname=update_user.firstname,
-                                   lastname=update_user.lastname,
-                                   age=update_user.age))
+    db.execute(update(User).where(User.id == user_id).values(firstname=update_user.firstname,
+                                                             lastname=update_user.lastname,
+                                                             age=update_user.age))
     db.commit()
     return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
 
 
 @router.delete('/delete')
 async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
-    user = db.scalars(select(User).where(User.id == user_id))
+    user = db.scalar(select(User).where(User.id == user_id))
     if user is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='User was not found'
         )
-    db.execute(update(User).where(User.id == user_id))
+    db.execute(delete(User).where(User.id == user_id))
     db.commit()
     return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
