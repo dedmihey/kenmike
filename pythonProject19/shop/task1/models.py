b431a1d795f4ffec98c8357c32079d814e2bf92c
dedmihey <164859123+dedmihey@users.noreply.github.com>
2024-12-03 20:38:06 +0300

    Add files via upload

--- pythonProject19/shop/task1/models.py
+++ pythonProject19/shop/task1/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
 class Buyer(models.Model):
-    name=models.CharField(max_length=20)
-    balance=models.DecimalField(decimal_places=2, max_digits=8, default='default balance')
-    age=models.IntegerField(default='default age')
+    name = models.CharField(max_length=20)
+    balance = models.DecimalField(max_digits=8, decimal_places=2, default='default balance')
+    age = models.IntegerField(default='default age')
 
 
 class Game(models.Model):
-    title=models.CharField(max_length=20)
-    cost=models.DecimalField(decimal_places=2, max_digits=8, default='default cost')
-    size=models.IntegerField(default='default size')
-    description=models.TextField(default='default description')
-    age_limited=models.BooleanField(default=False)
-    buyer=models.ManyToManyField(Buyer, related_name='games')
+    title = models.CharField(max_length=20)
+    cost = models.DecimalField(max_digits=8, decimal_places=2, default='default cost')
+    size = models.IntegerField(default='default size')
+    description = models.TextField(default='default description')
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games')
